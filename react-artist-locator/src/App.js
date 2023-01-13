0d82b876be474e5bea39476b62bb382f15544856
Andre Martinez <andre_martinez15@yahoo.com>
2023-01-12 21:48:18 -0800

    added functionality and video to herosection

--- react-artist-locator/src/App.js
+++ react-artist-locator/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 
@@ -10,9 +10,9 @@ function App() {
   return (
     <>
     <Router>
-    <Navbar></Navbar>
+    <Navbar />
     <Routes>
-    <Route path='/'exact component= {Home} />
+    <Route exact path='/' element= {<Home/>} />
     </Routes>
     </Router>
     </>
--- react-artist-locator/src/components/HeroSection.js
+++ react-artist-locator/src/components/HeroSection.js
@@ -1,11 +1,12 @@
 import React from "react";
+import '../App.css';
 import { Button } from "./Button";
 import "./HeroSection.css";
 
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-2.mp4" autoPlay loop muted />
+      <video src="/videos/2019-vid.mp4" autoPlay loop muted />
       <h1>YOUR FAVORITE ARTISTS</h1>
       <p>Within the reach of your hand.</p>
       <div className="hero-btns">
@@ -21,7 +22,7 @@ function HeroSection() {
           buttonStyle="btn--primary"
           buttonSize="btn--large"
         >
-          Watch Trailer<i className="far -fa-play-circle"/>
+          Watch Trailer<i className="far fa-play-circle"/>
         </Button>
       </div>
     </div>
